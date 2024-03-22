a05ac5d73321d76a37900637c19461113a495f9c
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-03-22 00:30:21 -0400

    got the first working version of the app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
+import requests
 
 app = Flask(__name__)
 
@@ -7,12 +8,44 @@ app.config['SECRET_KEY'] = "oh-so-secret"
 
 debug = DebugToolbarExtension(app)
 
-access_key = "2e0bceb90f226f7e6e4a4534e549f722"
-endpoint = 'convert'
+access_key = "b1a53257e5596240e73d3b7f"
+base_url = 'https://v6.exchangerate-api.com/v6/b1a53257e5596240e73d3b7f/pair/'
 
-@app.route('/')
+
+@app.route("/")
 def show_home_page():
     return render_template("base.html")
+
+# @app.route("/", methods = ["POST"])
+# def home_page():
+#     from_currency = request.form['from']
+#     to_currency = request.form['to']
+#     amount = request.form['amount']
+#     return render_template("base.html", from_currency = from_currency, to_currency = to_currency, amount = amount )
+
+@app.route("/", methods=["GET", "POST"])
+def home_page():
+    if request.method == "POST":
+        from_currency = request.form['from']
+        to_currency = request.form['to']
+        amount = float(request.form['amount'])  # Convert amount to float
+
+        # Make API call to get currency conversion
+        response = requests.get(f"{base_url}{from_currency}/{to_currency}/{amount}")
+        if response.status_code == 200:
+            data = response.json()
+            if data['result'] == 'success':
+                conversion_rate = data['conversion_rate']
+                conversion_result = data['conversion_result']
+                return render_template("base.html", from_currency=from_currency, to_currency=to_currency,amount=amount, conversion_rate=conversion_rate, conversion_result=conversion_result)
+            else:
+                error_message = "Currency conversion failed. Please try again later."
+                return render_template("base.html", error_message=error_message)
+        else:
+            error_message = f"Error {response.status_code}: Currency conversion request failed."
+            return render_template("base.html", error_message=error_message)
+    else:
+        return render_template("base.html")
 
 
 
